fa7a32f3cb80eb4296723a733cfbfbda6c38ccd4
skillnon2021-creator <skillnon2021@gmail.com>
2025-11-14 19:47:53 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,14 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 const { Client, GatewayIntentBits, EmbedBuilder, AttachmentBuilder, PermissionFlagsBits, ActivityType } = require('discord.js');
 const { createCanvas, loadImage, registerFont } = require('canvas');
 const fs = require('fs');
